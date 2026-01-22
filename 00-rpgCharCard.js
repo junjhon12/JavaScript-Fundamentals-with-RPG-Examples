db2cd8eef530f4348a048fbbe82d9283dc8decd4
junjhon12 <q.le102100@gmail.com>
2026-01-22 05:47:39 -0500

    feat: add initial RPG character card script
    Create `00-rpgCharCard.js` to demonstrate basic JavaScript variable declaration and console output.
    
    - Initialize constants for character name and class.
    - Define mutable variables for health points and quest status.
    - Implement basic arithmetic to simulate health reduction.
    - Output formatted character details to the console using template literals.

---
+++ 00-rpgCharCard.js
@@ -0,0 +1,10 @@
+const charName = "Brook";
+const charClass = "Ranger";
+let healthPoints = 100;
+let activeQuests = false;
+healthPoints -= 90;
+console.log(`
+    Name - ${charName}\n
+    Class - ${charClass}\n
+    HP - ${healthPoints}\n
+    Is Quest active? - ${activeQuests}`)
